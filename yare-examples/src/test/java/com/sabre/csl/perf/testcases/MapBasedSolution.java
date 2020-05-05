04b860ff7b5718908efb6a763adea29086877003
Prasad Gowda <bama_boy@DECCQDNQ2.iamsabre.sabrenow.com>
2020-05-05 20:48:47 +0530

    Added parallel execution

--- yare-examples/src/test/java/com/sabre/csl/perf/testcases/MapBasedSolution.java
+++ yare-examples/src/test/java/com/sabre/csl/perf/testcases/MapBasedSolution.java
@@ -24,6 +24,7 @@
 
 package com.sabre.csl.perf.testcases;
 
+import com.google.common.collect.Lists;
 import com.sabre.csl.perf.Action.Actions;
 import com.sabre.csl.perf.model.HotelFact;
 import com.sabre.csl.perf.model.HotelMapFact;
@@ -43,6 +44,9 @@ import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static com.sabre.oss.yare.invoker.java.MethodCallMetadata.method;
@@ -72,6 +76,21 @@ public class MapBasedSolution implements AbstractRuleTest {
                 .build();
 
         session = engine.createSession("hotels");
+    }
+
+    @Test
+    public void parallelRuleExecution() throws InterruptedException {
+        List<Long> averageTime = new ArrayList<>();
+        Instant start = Instant.now();
+        List<HotelFact> facts = getHotelFacts();
+        List<List<HotelFact>> partitions = Lists.partition(facts, 1000);
+        ExecutorService executorService = Executors.newFixedThreadPool(partitions.size());
+        for (List<HotelFact> hotelFacts : partitions)
+            executorService.execute(() -> checkProperty(hotelFacts,averageTime));
+        executorService.shutdown();
+        executorService.awaitTermination(120, TimeUnit.SECONDS);
+        Instant end = Instant.now();
+        System.out.println("End  -> " + Duration.between(start, end).toMillis() + " ms");
     }
 
     @Test
